2b398766f2b9179c426d42514f342595846ea1be
Michael Prokopchuk <prokopcm@gmail.com>
2016-01-28 16:06:12 -0500

    update tradegame

--- projects/trade_v0.2/js/systems/TransferSystem.js
+++
@@ -1,8 +0,0 @@
-var Trade = window.Trade || {};
-Trade.Systems = Trade.Systems || {};
-
-Trade.Systems.TransferSystem = function(options) {
-    this.type = "transfer";
-
-    this.parent = options.parent;
-};
