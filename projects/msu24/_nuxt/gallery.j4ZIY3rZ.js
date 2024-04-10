5ac5de930adb8420ade1f0935c05601dfc80b77d
Michael Prokopchuk <prokopcm@gmail.com>
2024-04-09 21:47:18 -0400

    Add skeleton for MSU scifest 24 project

---
+++ projects/msu24/_nuxt/gallery.j4ZIY3rZ.js
@@ -0,0 +1,1 @@
+import{_ as c,a as s}from"./Footer.puOvtuze.js";import{_,c as a,b as e,a as o,o as r}from"./entry.WqVj7nQ1.js";import"./nuxt-link.ClnuLl0G.js";const l={},m=o("section",null,[o("div",{class:"subhero-text"},"Gallery coming soon!")],-1);function p(i,d){const t=c,n=s;return r(),a("div",null,[e(t,{selected:"gallery"}),m,e(n)])}const g=_(l,[["render",p]]);export{g as default};
